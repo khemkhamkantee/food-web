7028b39a0b7618abc117f200c6eae459e8f3df67
DESKTOP-GGPJMJD\shabu <shabuwilly@gmail.com>
2020-12-09 15:58:39 +0700

    Update connectAPI.php

--- main/connectAPI.php
+++ main/connectAPI.php
@@ -1,6 +1,6 @@
 <?php 
     function getAPI($url){
-        $baseurl = 'http://459620411229.ngrok.io/api/';
+        $baseurl = 'http://23d7ff5be42a.ngrok.io/api/';
         // create & initialize a curl session
         $curl = curl_init();
 
@@ -21,7 +21,7 @@
     }
 
     function postAPI($url, $data){
-        $baseurl = 'http://459620411229.ngrok.io/api/';
+        $baseurl = 'http://23d7ff5be42a.ngrok.io/api/';
         // create & initialize a curl session
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_POST, 1);
